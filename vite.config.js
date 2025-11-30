a73259cdcface6383f486e50681e6fa301e1486e
ahmebyacine <ahmed.belaid1921@gmail.com>
2025-11-30 17:46:42 +0100

    fix: ensure maximum file size for caching in PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -39,6 +39,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg,jpeg}"],
       },
     }),
